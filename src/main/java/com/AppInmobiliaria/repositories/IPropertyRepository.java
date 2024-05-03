3b01399e8108dbe3c8dead52aea95b8261b4b355
damianrein <damianrein464@gmail.com>
2024-05-03 02:01:50 -0300

    commit

--- src/main/java/com/AppInmobiliaria/repositories/IPropertyRepository.java
+++ src/main/java/com/AppInmobiliaria/repositories/IPropertyRepository.java
@@ -12,9 +12,9 @@ import com.AppInmobiliaria.entities.Property;
 @Repository
 public interface IPropertyRepository extends JpaRepository<Property, Long>{
 
-	@Query("SELECT * Property AS p WHERE p.ciudad := ciudad AND p.state := FOR_RENT")
+	@Query("SELECT p FROM Property p WHERE p.ciudad = :ciudad AND p.state = 'FOR_RENT'")
 	List<Property> findPropertyByContratoAndCiudad(@Param(value = "ciudad") String ciudad);
 	
-	@Query("SELECT * Property AS p WHERE p.tipo:=CASA_QUINTA AND p.pileta:= true")
+	@Query("SELECT p FROM Property p WHERE p.tipo = 'CASA_QUINTA' AND p.pileta = true")
 	List<Property> findPropertyTipoAndPileta();
 }
