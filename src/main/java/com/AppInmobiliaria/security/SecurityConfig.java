33423be3acc377ee8a3895b9eafbc4acb97de8fe
damianrein <damianrein464@gmail.com>
2024-05-10 22:22:33 -0300

    1.0.0

--- src/main/java/com/AppInmobiliaria/security/SecurityConfig.java
+++ src/main/java/com/AppInmobiliaria/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.AppInmobiliaria.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -34,5 +39,17 @@ public class SecurityConfig {
 		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 		.build()
 		;
+	}
+	
+	@Bean
+	CorsConfigurationSource configCors() {
+		CorsConfiguration config = new CorsConfiguration();
+			config.setAllowedOriginPatterns(List.of("http://localhost:4200"));
+			config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+			config.setAllowedMethods(List.of("POST","PUT","GET","DELETE"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 }
